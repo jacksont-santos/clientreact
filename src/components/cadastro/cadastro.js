7e0884fff76b608591897ef8ce60dc2f42c5e1cb
Jackson <jack0702@hotmail.com.br>
2021-12-21 03:05:06 -0300

    modal pronto

--- src/components/cadastro/cadastro.js
+++ src/components/cadastro/cadastro.js
@@ -1,7 +1,61 @@
-const Cadastro = () => {
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./cad.css";
+
+const Cadastro = (props) => {
+  const navigate = useNavigate();
+
+  const modal = props.modal;
+
+
+  const handleSubmit = async (evento) => {
+    evento.preventDefault();
+    const name = evento.target.name.value;
+    const email = evento.target.email.value;
+    const birthdate = evento.target.birthdate.value;
+    const password = evento.target.password.value;
+    const passwordConfirmation = evento.target.confirmacao.value;
+    const imageUrl = evento.target.imageUrl.value;
+
+    const newUser = {
+      name,
+      email,
+      birthdate,
+      password,
+      passwordConfirmation,
+      imageUrl,
+    };
+
+    await axios
+      .post("/user/create", newUser)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+      modal();
+    navigate("/");
+  };
+
+  const esvaziar = () => {
+    let name = document.getElementById("name");
+    let email = document.getElementById("email");
+    let birthdate = document.getElementById("birthdate");
+    let password = document.getElementById("password");
+    let passwordConfirmation = document.getElementById("confirmacao");
+    let imageUrl = document.getElementById("imageUrl");
+    name.value = "";
+    email.value = "";
+    birthdate.value = "";
+    password.value = "";
+    passwordConfirmation.value = "";
+    imageUrl.value = "";
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
           name="name"
@@ -23,7 +77,7 @@ const Cadastro = () => {
           <strong>Email</strong>
         </label>
         <input
-          type="data"
+          type="date"
           name="birthdate"
           id="birthdate"
           placeholder=" Digite sua data de nascimento"
@@ -52,8 +106,28 @@ const Cadastro = () => {
         <label htmlFor="confirmacao">
           <strong>Confirmação</strong>
         </label>
+        <input
+          type="text"
+          name="imageUrl"
+          id="imageUrl"
+          placeholder=" Digite a url da imagem"
+        />
+        <label htmlFor="imageUrl">
+          <strong>Imagem</strong>
+        </label>
+        <div className="justify-content">
+          <button type="submit" className="sucess">
+            <strong>Cadastrar</strong>
+          </button>
+        </div>
+        <div className="justify-content">
+          <button type="button" className="danger" onClick={esvaziar}>
+            <strong> Apagar tudo</strong>
+          </button>
+        </div>
       </form>
     </div>
   );
 };
+
 export default Cadastro;
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,7 +1,16 @@
 import "./home.css";
 import {Link} from "react-router-dom";
+import 'react-responsive-modal/styles.css';
+import { Modal } from 'react-responsive-modal';
+import { useState } from "react";
+import Cadastro from "../cadastro/cadastro";
 
 const Home = () => {
+
+    const [aberto, setAbrir] = useState(false);
+  const AbreModal = () => setAbrir(true);
+  const FechaModal = () => setAbrir(false);
+
     return (
         <div className="home">
             <div className="search">
@@ -14,16 +23,17 @@ const Home = () => {
                 <p> Essa é uma plataforma de streaming grátuita.</p>
             </div>
             <p className="cad"><strong> Não tem uma conta?</strong>
-            <Link to="/cadastro">
-                <strong className="strong"> Cadastre-se</strong>
-            </Link>
+                <strong className="strong" onClick={AbreModal}> Cadastre-se</strong>           
             </p>
             <Link to="/list">
                 <div className="homeList">
                     <h3> Aqui você pode Confirir a lista de filmes cadastrados.</h3>
                  </div>
-            </Link>      
+            </Link>
+            <Modal open={aberto} onClose={FechaModal} center showCloseIcon={false}>
+                <Cadastro modal={FechaModal}/>
+            </Modal>     
         </div>
-    )
+    );
 };
 export default Home;
